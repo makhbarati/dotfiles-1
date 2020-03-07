45abf86224c39983fcde35da722b8df6af77ceab
Steven Loria <sloria1@gmail.com>
2020-03-07 11:55:05 -0500

    Fix for installing python

--- roles/zsh/_env.zsh
+++ roles/zsh/_env.zsh
@@ -8,3 +8,8 @@ export PED_EDITOR='vim'
 export KONCH_EDITOR='vim'
 export FAM_SOURCE="$HOME/Dropbox/fam"
 export FAM_EDITOR="vim"
+
+# Needed for installing psycopg2
+# See `brew info openssl`
+export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
